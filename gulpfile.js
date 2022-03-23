f043da2272309edae12927572897ba481ff1346c
Cigarrito RD <maycolk.hernandez@gmail.com>
2022-03-23 19:36:53 -0400

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const {src, dest, watch, parallel, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const webp = require('gulp-webp');
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
+
+function css() {
+    return src('src/scss/**/*.scss') 
+        .pipe(sass())
+        .pipe( dest('./build/css') );
+
+   
+}
+
+ function sassFile() {
+   return src('src/**/*.sass')
+    .pipe(sass())
+    .pipe(dest('.build/css/'));
+      
+}
+
+function javascript() {
+    return src('src/js/**/*.js')
+    //   .pipe(sourcemaps.init())
+    //   .pipe(concat('bundle.js')) // final output file name
+    //   .pipe(terser())
+    //   .pipe(sourcemaps.write('.'))
+    //   .pipe(rename({ suffix: '.min' }))
+      .pipe(dest('./build/js'))
+     
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe(cache(imagemin({ optimizationLevel: 3})))
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+
+}
+
+function versionWebp() {
+    return src('src/img/**/*')
+        .pipe( webp() )
+        .pipe(dest('build/img'))
+        .pipe(notify({ message: 'Imagen Completada'}));
+        
+}
+function watchFiles() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/**/*.sass', sassFile)
+    watch('src/**/*.js', javascript);
+    watch('src/img/**/*', imagenes);
+    watch('src/img/**/*', versionWebp);
+    
+}
+
+exports.default = parallel(sassFile, css, javascript, imagenes, versionWebp, watchFiles);
